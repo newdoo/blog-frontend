d4e99694d0318d05e13751b47868a20d7e93be80
DESKTOP-71BE5MQ\doo <doo8866@numixent.com>
2018-09-20 15:34:54 +0900

    Editor 페이지 구성

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import EditorTemplate from '../components/editor/EditorTemplate/EditorTemplate';
+import EditorHeader from '../components/editor/EditorHeader/EditorHeader';
+import EditorPane from '../components/editor/EditorPane/EditorPane';
+import PreviewPane from '../components/editor/PreviewPane';
 
 const EditorPage = () => {
     return (
-        <div>
-            Editor
-        </div>
+        <EditorTemplate
+            header={<EditorHeader/>}
+            editor={<EditorPane/>}
+            preview={<PreviewPane/>}
+        />
     );
 };
 
